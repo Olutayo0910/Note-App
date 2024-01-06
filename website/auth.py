a119c3c7013134e30bdcb020f31a7441dd09ddab
Olutayo <olutayoogunlade2022@gmail.com>
2024-01-06 15:50:34 +0100

    updated login manager

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from . import db
 from .models import User
-from flask_bcrypt import Bcrypt, check_password_hash
-
+from flask_bcrypt import Bcrypt
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
@@ -18,16 +18,20 @@ def login():
         if user:
             if bcrypt.check_password_hash(user.password, password):
                 flash('Logged in Succesfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect Password. Try again!', category='error')
         else:
             flash('User does not exist', category='error')
 
-    return render_template('login.html', boolean=True)
+    return render_template('login.html', user=current_user )
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -54,5 +58,6 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created successfully!', category='succcess')
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
